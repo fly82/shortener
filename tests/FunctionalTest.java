f5a12fb1e72feae1af25a641496cd03b8f5969be
vadym <fly82@ukr.net>
2017-08-10 13:33:26 +0200

    finish

---
+++ tests/FunctionalTest.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task33.task3310.tests;
+
+import com.javarush.task.task33.task3310.Shortener;
+import org.junit.Test;
+
+public class FunctionalTest {
+
+    @Test
+    void testStorage(Shortener shortener) {
+
+    }
+}
